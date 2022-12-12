e61c6bf1085baae85d457dd0c7451d02c99c6829
serhii-sultanov <sshsultanov@gmail.com>
2022-12-12 13:24:04 +0200

    created getDiff function with TypeScript

---
+++ build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+const getDiff = (initialValue, valueToCompare) => {
+    const initialArray = initialValue.split("");
+    const arrayToCompare = valueToCompare.split("");
+    const difference = initialArray
+        .filter(s => !arrayToCompare.includes(s))
+        .concat(arrayToCompare.filter(s => !initialArray.includes(s)));
+    const result = {
+        amount: difference.length,
+        array: difference
+    };
+    return result;
+};
+console.log(getDiff('abc', 'abcd')); // { amount: 1, array: ['d'] }
+console.log(getDiff('abcd', 'cdfe')); // { amount: 4, array: ['a', 'b', 'f', 'e'] }
+console.log(getDiff('abc', 'wbbcc')); // { amount: 2, array: ['a', 'w'] }
+console.log(getDiff('abc', 'abc')); // { amount: 0, array: [] }
